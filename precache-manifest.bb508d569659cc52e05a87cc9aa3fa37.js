e94b1e0454b426d34ec337213a383ceec0583a54
Anuj Gupta <anujgupt15@gmail.com>
2021-10-29 00:42:19 +0530

    Updates

---
+++ precache-manifest.bb508d569659cc52e05a87cc9aa3fa37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1538731c79ad7c87851f653df6a8fab",
+    "url": "/Contact-Pedia/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/Contact-Pedia/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "37fa74c40189f959fc98",
+    "url": "/Contact-Pedia/static/js/main.4ff3f723.chunk.js"
+  },
+  {
+    "revision": "c99d706b4a2b078cfc44",
+    "url": "/Contact-Pedia/static/js/runtime-main.392d4317.js"
+  }
+]);
